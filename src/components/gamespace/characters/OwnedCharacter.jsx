e8bb2ee9f458f1cf64881ab0c4422d1b8746615c
Fyodor <betterideass8810@gmail.com>
2024-03-20 08:04:10 +0000

    tmp

--- src/components/gamespace/characters/OwnedCharacter.jsx
+++ src/components/gamespace/characters/OwnedCharacter.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
 
-const OwnedCharacter = () => {
+const OwnedCharacter = ({gameFieldContext, avatar, attributes, position}) => {
+    console.log(avatar)
     return (
-        <div className="character">
+        <div className="owned-character"
+             style={{
+                 left: position.x * gameFieldContext.cellSize,
+                 top: position.y * gameFieldContext.cellSize,
+                 width: gameFieldContext.cellSize,
+                 height: gameFieldContext.cellSize,
+                 padding: gameFieldContext.cellSize * 0.1
+             }}>
+            <img src={avatar} style={{width: gameFieldContext.cellSize * 0.8, height: gameFieldContext.cellSize * 0.8}}
+                 alt={'aboba'}/>
 
         </div>
     );
--- src/components/gamespace/gamefield/GameField.jsx
+++ src/components/gamespace/gamefield/GameField.jsx
@@ -1,10 +1,13 @@
-import React, {useRef} from 'react';
+import React, {useRef, useState} from 'react';
 import '../gamespace.css'
 import {useFetchGameFieldData} from "../../../api/game/fetchGameFieldData";
 import {devConsts} from "../../../util/util";
 import Character from "../characters/Character";
+import OwnedCharacter from "../characters/OwnedCharacter";
 
 const GameField = ({children}) => {
+    const [waitingForMove, setWaitingForMove] = useState(false);
+
     const fieldRef = useRef(null);
     const data = useFetchGameFieldData();
     const fieldWidth = devConsts.defaultCellSize * data?.size[0];
@@ -12,14 +15,18 @@ const GameField = ({children}) => {
     const fieldContext = {
         fieldWidth,
         fieldHeigth,
-        cellSize: devConsts.defaultCellSize
+        setWaitingForMove,
+        cellSize: devConsts.defaultCellSize,
     }
+
+
     return (
         <div className="game-field"
              style={{backgroundImage: `url(${data?.background})`, width: fieldWidth, height: fieldHeigth}}
              ref={fieldRef}>
-            {data?.characters.map((character) => <Character gameFieldContext={fieldContext} position={character.position}
-                                                           attributes={character.attributes} avatar={character.avatar}/>)}
+            {data?.characters.map((character) => <Character gameFieldContext={fieldContext} {...character}/>)}
+            {data?.ownedCharacters.map((character) => <OwnedCharacter gameFieldContext={fieldContext} {...character}/>)}
+
         </div>
     );
 };
