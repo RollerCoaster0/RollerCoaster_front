bd1c7ce9d5624ea2e2f5eb25d9a657dd7c93e31f
fyodor <betterideass8810@gmail.com>
2024-03-21 21:38:17 +0000

    transition added

--- src/components/gamespace/characters/Character.jsx
+++ src/components/gamespace/characters/Character.jsx
@@ -5,16 +5,14 @@ import {GameContext} from "../../GameContext";
 const Character = ({id, avatar, attributes}) => {
     const {field, fieldParams, setPickedCharacter} = useContext(GameContext);
     const position = getPosition(field, fieldParams, id);
-    // console.log(position)
     return (
         <div className="character"
              style={{
-                 left: position.x,
-                 top: position.y,
+                 transform: `translate(${position.x}px, ${position.y}px)`,
                  width: fieldParams?.cellSize,
                  height: fieldParams?.cellSize,
                  padding: fieldParams?.cellSize * 0.1
-             }} onClick={() => setPickedCharacter(id)}>
+             }} onClick={(e) => {setPickedCharacter(id); e.stopPropagation();}}>
             <img src={avatar} style={{width: fieldParams?.cellSize * 0.8, height: fieldParams?.cellSize * 0.8}}
                  alt={'aboba'}/>
 
@@ -27,11 +25,11 @@ function getPosition(field, fieldParams, id) {
     for (let i = 0; i < field?.length; i++) {
         for (let j = 0; j < field[i].length; j++) {
             if (field[i][j] === id) {
-                // console.log(i, j)
                 return {x: j * fieldParams?.cellSize, y: i * fieldParams?.cellSize}
             }
         }
     }
+    return null;
 }
 
 export default Character;
