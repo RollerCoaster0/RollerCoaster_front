1f1afbaac69d8dc560a0a68ad77771be0e7aeefb
Николай <kolabor31@gmail.com>
2024-03-23 14:57:03 +0300

    попытка сделать похожее на правду

---
+++ src/components/pages/rega/Okno_rega.jsx
@@ -0,0 +1,34 @@
+import React, {useState} from 'react';//поле Реги
+
+const PasswordCheck = () => {
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [match, setMatch] = useState(null);
+
+    const handlePasswordChange = (e) => {
+        setPassword(e.target.value);
+    }
+
+    const handleConfirmPasswordChange = (e) => {
+        setConfirmPassword(e.target.value);
+        if (e.target.value === password) {
+            setMatch(true);
+        } else {
+            setMatch(false);
+        }
+    }
+
+    return (
+        <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh'}}>
+            <div>
+                <input type="text" placeholder="Имя/nickname"/><br/>
+                <input type="password" placeholder="Password" value={password} onChange={handlePasswordChange}/><br/>
+                <input type="password" placeholder="Повторите Password" value={confirmPassword}
+                       onChange={handleConfirmPasswordChange}/>
+                {match === true ? <span>&#10004;</span> : match === false ? <span>&#10060;</span> : null}
+            </div>
+        </div>
+    );
+}
+
+export default PasswordCheck;
