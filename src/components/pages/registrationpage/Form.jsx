ed9f82990ddd95659179e4052cdf6dc39f5cea67
Shakur <dimapak2022004@gmail.com>
2024-03-24 13:49:53 +0300

    changed the name of the component Okno_rega(Authentication) added style.css, added password check in RegistrationPage

--- src/components/pages/registrationpage/Form.jsx
+++ src/components/pages/registrationpage/Form.jsx
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './styles.css'
 import Button from "./Button";
+
 const Form = () => {
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [match, setMatch] = useState(null);
+
+    const handlePasswordChange = (e) => {
+        setPassword(e.target.value);
+    }
+
+    const handleConfirmPasswordChange = (e) => {
+        setConfirmPassword(e.target.value);
+        if (e.target.value === password) {
+            setMatch(true);
+        } else {
+            setMatch(false);
+        }
+    }
     return (
         <>
             <div className="register-form-container">
@@ -11,7 +28,10 @@ const Form = () => {
                         <input type="text" placeholder="NickName"/>
                     </div>
                     <div className="form-field">
-                        <input type="password" placeholder="Password"/>
+                        <input type="password" placeholder="Password" value={password} onChange={handlePasswordChange}/><br/>
+                        <input type="password" placeholder="Повторите Password" value={confirmPassword}
+                               onChange={handleConfirmPasswordChange}/>
+                        {match === true ? <span>&#10004;</span> : match === false ? <span>&#10060;</span> : null}
                     </div>
                     <div className="form-field">
                         <input type="text" placeholder="Email"/>
@@ -19,7 +39,6 @@ const Form = () => {
                 </div>
                 <Button></Button>
             </div>
-
         </>
     );
 };
--- src/components/pages/registrationpage/RegistrationPage.jsx
+++ src/components/pages/registrationpage/RegistrationPage.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import './styles.css'
 import Form from "./Form";
+import Authentication from "../authentication/Authentication";
 
 const RegistrationPage = () => {
     return (
         <>
             <main>
-        <Form></Form>
+                <Form></Form>
             </main>
         </>
     );
