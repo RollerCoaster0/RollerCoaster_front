7182db90fe907fad24ccde0dd5c69dac362aa7d9
Николай <kolabor31@gmail.com>
2024-03-30 10:24:17 +0300

    postpage-окно после регистрации

---
+++ src/components/pages/postpage/postpage.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Navbar from "../../common/navbar/Navbar";
+
+const postpage = () => {
+    return (<>
+        <Navbar/>
+
+        <div className="postpage">
+            <div className="Top-row">
+                <h2 class="color">Известные игроки и мастера</h2>
+                <p>Список с авто обновлением</p>
+                </div>
+            /*здесь нужно сделать поиск и тп*/
+        </div>
+            </>
+            );
+            };
