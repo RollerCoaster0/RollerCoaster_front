6da61bd0e7e4bbfff63fb52a6d64479133d3d3e3
Николай <kolabor31@gmail.com>
2024-04-09 20:00:54 +0300

    попытка g

---
+++ src/components/pages/homepage/Homepage.test.jsx
@@ -0,0 +1,9 @@
+import {React} from "react";
+import {render} from "@testing-library/react";
+import HomePage from "./HomePage";
+
+/*test('Тест на Homepage', ()=>{
+    render(<HomePage />);
+    const content__grid_item1 = screen.getByText(/Добро пожаловать в мир приключений и фэнтези!/i)
+    expect(content__grid_item1).toBeInTheDocument();
+});*/
