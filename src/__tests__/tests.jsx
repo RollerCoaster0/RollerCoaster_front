d6df62eddb2c0c5a4e682131f82dfd598bb46dad
Shakur <dimapak2022004@gmail.com>
2024-04-14 12:05:10 +0300

    commit dependencies of jest

--- src/__tests__/tests.jsx
+++ src/__tests__/tests.jsx
@@ -1,9 +1,12 @@
-import {React} from "react";
+import React from "react";
 import {render} from "@testing-library/react";
-import HomePage from "./HomePage";
+import HomePage from "../components/pages/homepage/HomePage";
 
 test('tests for Homepage', ()=>{
     render(<HomePage />);
     const content = screen.getByText(content)
     expect(content).toBeInTheDocument();
+});
+test('My first test', () => {
+    expect(Math.max(1, 5, 10)).toBe(10);
 });
