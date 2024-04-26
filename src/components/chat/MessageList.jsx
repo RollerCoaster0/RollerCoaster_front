019f24de912792837a38a82c9fab8db1653f7e11
Shakur <dimapak2022004@gmail.com>
2024-04-27 00:12:42 +0300

    chat

--- src/components/chat/MessageList.jsx
+++ src/components/chat/MessageList.jsx
@@ -1,26 +1,29 @@
-
+import img from "./img/img.png"
 const MessageList = ({messages}) => (
-
         <ul style={{
             position: "relative",
             display: "flex",
             flexDirection:"column",
             gap:"10px",
-            alignItems:"end"
+            maxWidth:"250px",
+            alignItems:"end",
+            listStyle:"none",
 
         }}>
             {messages.map((message, index) => (
-
+                    <div className="message">
                     <div className="message_field" key={index}>
                         <li style={{
 
-
+                            maxWidth:"250px"
                         }}>{message}</li>
                     </div>
 
+                        <div>
+                        <img className="message_field__avavtar" src={img} /*onClick={() => navigate('/')} style={{cursor: 'pointer'}*//>
+                        </div>
+                    </div>
             ))}
         </ul>
 );
-
-
 export default MessageList;
