08c04d16a8684efc3af78b9fd8fbdbcfa5ae21eb
Fyodor <betterideass8810@gmail.com>
2024-05-19 21:13:41 +0300

    retarder zoom

--- src/components/gamespace/gamefield/GameField.jsx
+++ src/components/gamespace/gamefield/GameField.jsx
@@ -3,10 +3,14 @@ import '../gamespace.css'
 import Character from "../characters/Character";
 import {GameContext, gamePhaseType} from "../../../contexts/GameContext";
 import {devConsts} from "../../../util/util";
+import {useZoom} from "../../../hooks/useZoom";
 
 const GameField = ({children}) => {
+    const {setCellSize} = useContext(GameContext)
+    const fieldRef = useRef()
+    useZoom(fieldRef, devConsts.defaultCellSize, setCellSize)
     return (
-        <div  style={{width: 9000, height: 5000}} >
+        <div ref={fieldRef} style={{width: 9000, height: 5000}} >
             {children}
         </div>
     )
